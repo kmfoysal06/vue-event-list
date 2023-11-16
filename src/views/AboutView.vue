a66186d84ba565c047ebabafd642b735337ea207
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-16 19:42:55 +0600

    updated about component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>Events For Making A Great World Together</h1>
+    <h4>Events For Making A Great World Together</h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ducimus quisquam eaque praesentium maxime cupiditate recusandae repellat ad! Temporibus iste molestias ipsa totam deleniti? Ea minus voluptatibus optio eveniet quidem rem assumenda! Reprehenderit aut tempore quasi eligendi aliquam in exercitationem?
+    </p>
   </div>
 </template>
