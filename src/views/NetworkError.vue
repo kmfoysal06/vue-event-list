d3500a79c683a9b43254e3aa40ba16d10756d33d
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-16 19:38:59 +0600

    updated networkerror component

--- src/views/NetworkError.vue
+++ src/views/NetworkError.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>Oops Youre Not Connected</h1>
+  <h1>OH!</h1>
+  <h2>Oops Youre Not Connected</h2>
   <h3>
     it seems you are having some network issue.take a breath go to
     <a @click="$router.go(-1)">back</a> and try again
